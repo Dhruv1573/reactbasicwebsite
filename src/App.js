7a62f9b18cdb122c08908c039ea762872d820cac
Dhruv kumar <36184412+Dhruv1573@users.noreply.github.com>
2023-05-06 11:13:56 +0530

    finalizing the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import Contact from './components/Contact';
+import Services from './components/Services';
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss"
+import "./styles/Footer.scss"
+import "./styles/Contact.scss"
+import "./styles/mediaquery.scss"
 function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/services' element={<Services/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {HashLink} from "react-router-hash-link"
 const Header = () => {
   return (
    <nav>
         <h1>TechyStar.</h1>
         <main>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/#about"}>About</Link>
+            <HashLink to={"/#home"}>Home</HashLink>
+            <HashLink to={"/#about"}>About</HashLink>
             <Link to={"/contact"}>Contact</Link>
-            <Link to={"/#brands"}>Brands</Link>
-            <Link to={"/services"}>Services</Link>
+            <HashLink to={"/#brands"}>Brands</HashLink>
+            <HashLink to={"/services"}>Services</HashLink>
         </main>
    </nav>
   )
